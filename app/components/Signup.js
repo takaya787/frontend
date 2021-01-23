b6a79afa54e64e8c7fe3b8f5e2e35db3c6481ea4
takaya787 <takaya318318@gmail.com>
2021-01-23 14:44:57 +0900

    signup switchでrenderする箇所を最小化

--- app/components/Signup.js
+++ app/components/Signup.js
@@ -8,45 +8,54 @@ import LoginForm from './headers/LoginForm';
 export default function Signup(props) {
   const [isSignup, setIsSignup] = useState(false);
   const [isLogin, setIsLogin] = useState(false);
+  //formの開け締めを管理する
+  const [isOpen, setIsOpen] = useState(false);
   const Closemodal = () => {
     setIsSignup(false);
     setIsLogin(false);
+    setIsOpen(false);
   }
   const Signupcontroll = () => {
     setIsSignup(!isSignup);
     setIsLogin(false);
+    setIsOpen(true);
   }
   const Logincontroll = () => {
     setIsSignup(false);
     setIsLogin(!isLogin);
+    setIsOpen(true);
   }
   return (
     <>{/* 始めはボタンが表示される */}
-      { !isSignup && !isLogin &&
+      { !isOpen &&
         (<button onClick={Signupcontroll} className={styles.initial}>{props.title}</button>)
       }
+
       {/* Sign up用のformが表示される */}
-      { isSignup && !isLogin &&
+      { isOpen &&
         (<div className={styles.modal}>
           <div className={styles.content}>
             <button className={styles.content_close} onClick={Closemodal}>×</button>
-            <h2 className={styles.content_title}>ユーザー登録！</h2>
-            <UserForm />
-            <button className={styles.content_switch} onClick={Logincontroll}>ログインはこちら</button>
-          </div>
-        </div>)
-      }
-      {/* Login用のformが表示される */}
-      { !isSignup && isLogin && (
-        <div className={styles.modal}>
-          <div className={styles.content}>
-            <button className={styles.content_close} onClick={Closemodal}>×</button>
-            <h2 className={styles.content_title}>Log inはこちら！</h2>
-            <LoginForm />
-            <button className={styles.content_switch} onClick={Signupcontroll}>ユーザー登録はこちら</button>
+            {/* signup用のformが表示される */}
+            {isSignup && (
+              <>
+                <h2 className={styles.content_title}>ユーザー登録！</h2>
+                <UserForm />
+                <button className={styles.content_switch} onClick={Logincontroll}>ログインはこちら</button>
+              </>
+            )}
+            {/* Login用のformが表示される */}
+            {isLogin && (
+              <>
+                <h2 className={styles.content_title}>Log inはこちら！</h2>
+                <LoginForm />
+                <button className={styles.content_switch} onClick={Signupcontroll}>ユーザー登録はこちら</button>
+              </>
+            )}
           </div>
         </div>
-      )}
+        )
+      }
     </>
   )
 }
